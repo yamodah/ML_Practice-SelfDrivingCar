c9fbcd6e5f96a62222595bbaf6563b32b64dc530
yamodah <yassinedahlek@gmail.com>
2022-05-15 17:03:32 -0400

    debugged empty weights arrays and connected neural network to car movement

--- network.js
+++ network.js
@@ -2,16 +2,16 @@ class NeuralNetwork{
     constructor(neuronCounts){
         this.levels = []
         for(let i=0; i<neuronCounts.length-1;i++){
-            this.levels.push(new Level(neuronCountsi[i], neuronCounts[i+1]))
+            this.levels.push(new Level(neuronCounts[i], neuronCounts[i+1]))
         }
     }
     static feedForward(givenInputs, network){
         let outputs = Level.feedForward(
             givenInputs, network.levels[0]
         )
-        for (let i=1;i<network.lvels.length;i++){
+        for (let i=1;i<network.levels.length;i++){
             outputs = Level.feedForward(
-                outputs, network.level[i]
+                outputs, network.levels[i]
             )
         }
         return outputs
@@ -32,16 +32,19 @@ class Level{
     }
 
     static #randomize(level){
-        for(let i=0; i<level.inputCount; i++){
-            for(let j = 0 ; j<level.outputCount; j++){
+        for(let i=0; i<level.inputs.length; i++){
+
+            for(let j = 0 ; j<level.outputs.length; j++){
                 level.weights[i][j] = Math.random()*2 - 1
             }
         }
         for (let i=0; i<level.biases.length;i++){
             level.biases[i] = Math.random()*2-1
         }
+        console.log(level.weights)
     }
     static feedForward(givenInputs, level){
+
         for (let i=0; i<level.inputs.length;i++){
             level.inputs[i]= givenInputs[i]
         }
@@ -50,6 +53,7 @@ class Level{
             for (let j = 0; j < level.inputs.length; j++) {
                 sum+=level.inputs[j]*level.weights[j][i]
             }
+            
             if(sum>level.biases[i]){
                 level.outputs[i]=1
             }else{
