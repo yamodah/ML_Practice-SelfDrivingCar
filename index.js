3731dad7b4161f2383450a0e49bd565e27fb7537
yamodah <yassinedahlek@gmail.com>
2022-05-14 21:21:04 -0400

    added camera feel to the road

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const canvas = document.getElementById("myCanvas")
-canvas.width = 200
+const canvas = document.getElementById("myCanvas");
+canvas.width = 200;
 
-const ctx = canvas.getContext("2d")
-const road = new Road(canvas.width/2, canvas.width*0.9)
-const car = new Car(road.getLaneCenter(1),100,30,50)
+const ctx = canvas.getContext("2d");
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 
-animate()
+animate();
 
-function animate(){
-    car.update()
-    //move resize into animate functions like fillrect to clear the canvas
-    canvas.height = innerHeight
-    road.draw(ctx)
-    car.draw(ctx)
-    requestAnimationFrame(animate)
+function animate() {
+  car.update();
+  //move resize into animate functions like fillrect to clear the canvas
+  canvas.height = innerHeight;
+  ctx.save();
+  ctx.translate(0, -car.y + canvas.height * 0.7);
+  road.draw(ctx);
+  car.draw(ctx);
+
+  ctx.restore();
+  requestAnimationFrame(animate);
 }
