517275493db45ad69dd6d576dadb7b60821bb16e
yamodah <yassinedahlek@gmail.com>
2022-05-14 20:23:56 -0400

    moved moving logic into private method

--- car.js
+++ car.js
@@ -14,38 +14,41 @@ class Car {
     this.controls = new Controls();
   }
   update() {
+   this.#move()
+  }
+  #move(){
     if (this.controls.forward) {
-      this.speed += this.acceleration;
-    }
-    if (this.controls.reverse) {
-      this.speed -= this.acceleration;
-    }
-    if (this.speed > this.maxSpeed) {
-      this.speed = this.maxSpeed;
-    }
-    if (this.speed < -this.maxSpeed / 2) {
-      this.speed = -this.maxSpeed / 2;
-    }
-    if (this.speed > 0) {
-      this.speed -= this.friction;
-    }
-    if (this.speed < 0) {
-      this.speed += this.friction;
-    }
-    if (Math.abs(this.speed) < this.friction) {
-      this.speed = 0;
-    }
-    if(this.speed!=0){
-        const flip = this.speed>0?1:-1
-        if (this.controls.left) {
-          this.angle += 0.03*flip;
-        }
-        if (this.controls.right) {
-          this.angle -= 0.03*flip;
-        }
-    }
-    this.x -= Math.sin(this.angle) * this.speed;
-    this.y -= Math.cos(this.angle) * this.speed;
+        this.speed += this.acceleration;
+      }
+      if (this.controls.reverse) {
+        this.speed -= this.acceleration;
+      }
+      if (this.speed > this.maxSpeed) {
+        this.speed = this.maxSpeed;
+      }
+      if (this.speed < -this.maxSpeed / 2) {
+        this.speed = -this.maxSpeed / 2;
+      }
+      if (this.speed > 0) {
+        this.speed -= this.friction;
+      }
+      if (this.speed < 0) {
+        this.speed += this.friction;
+      }
+      if (Math.abs(this.speed) < this.friction) {
+        this.speed = 0;
+      }
+      if(this.speed!=0){
+          const flip = this.speed>0?1:-1
+          if (this.controls.left) {
+            this.angle += 0.03*flip;
+          }
+          if (this.controls.right) {
+            this.angle -= 0.03*flip;
+          }
+      }
+      this.x -= Math.sin(this.angle) * this.speed;
+      this.y -= Math.cos(this.angle) * this.speed;
   }
   draw(ctx) {
     ctx.save();
