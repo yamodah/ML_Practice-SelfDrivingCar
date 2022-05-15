be16b4c1786262d8f7f60fa70570fb2a9da023f7
yamodah <yassinedahlek@gmail.com>
2022-05-15 13:58:56 -0400

    added coloring of cars

--- car.js
+++ car.js
@@ -102,11 +102,11 @@ class Car {
     this.x -= Math.sin(this.angle) * this.speed;
     this.y -= Math.cos(this.angle) * this.speed;
   }
-  draw(ctx) {
+  draw(ctx, color) {
     if (this.damaged) {
       ctx.fillStyle = "grey";
     } else {
-      ctx.fillStyle = "black";
+      ctx.fillStyle = color;
     }
     ctx.beginPath();
     ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
