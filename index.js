49b0ff852f1bac48d0241ed9b93fdff3013300c3
yamodah <yassinedahlek@gmail.com>
2022-05-16 10:48:51 -0400

    added ability to mutate and produce better cars from one gen to another

--- index.js
+++ index.js
@@ -9,29 +9,43 @@ const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 
 const N = 100;
 const cars = generateCars(N);
-let bestCar = cars[0]
-if(localStorage.getItem("bestBrain")){
-  bestCar.brain = JSON.parse(
-    localStorage.getItem("bestBrain")
-  )
+let bestCar = cars[0];
+if (localStorage.getItem("bestBrain")) {
+  for (let i = 0; i < cars.length; i++) {
+    cars[i].brain = JSON.parse(localStorage.getItem("bestBrain"));
+    if (i != 0) {
+      NeuralNetwork.mutate(cars[i].brain, 0.01);
+    }
+  }
 }
 
-const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2),
-new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY", 2),
-new Car(road.getLaneCenter(2), -300, 30, 50, "DUMMY", 2),
+const traffic = [
+  new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(2), -300, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(0), -500, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(1), -500, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(1), -700, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(2), -700, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(1), -1000, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(0), -1200, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(2), -1300, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(1), -1500, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(2), -1500, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(2), -1700, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(2), -1900, 30, 50, "DUMMY", 2),
 ];
 
 animate();
-function save(){
-  localStorage.setItem("bestBrain",
-  JSON.stringify(bestCar.brain))
+function save() {
+  localStorage.setItem("bestBrain", JSON.stringify(bestCar.brain));
 }
-function discard(){
-  localStorage.removeItem("bestBrain")
+function discard() {
+  localStorage.removeItem("bestBrain");
 }
 function generateCars(N) {
   const cars = [];
-  for (let i = 0; i <= N; i++) {
+  for (let i = 1; i <= N; i++) {
     cars.push(new Car(road.getLaneCenter(1), 100, 30, 50, "AI"));
   }
   return cars;
--- network.js
+++ network.js
@@ -16,6 +16,26 @@ class NeuralNetwork{
         }
         return outputs
     }
+    static mutate(network, amount=1){
+        network.levels.forEach((level)=>{
+            for (let i = 0; i < level.biases.length; i++) {
+                level.biases[i] = lerp(
+                    level.biases[i],
+                    Math.random()*2-1,
+                    amount
+                )
+            }
+            for (let i = 0; i < level.weights.length; i++) {
+                for(let j=0;j<level.weights[i].length;j++){
+                    level.weights[i][j] =lerp(
+                        level.weights[i][j],
+                        Math.random()*2-1,
+                        amount
+                    )
+                }
+            }
+        })
+    }
 }
 class Level{
     constructor(inputCount, outputCount){
@@ -61,4 +81,5 @@ class Level{
         }
         return level.outputs
     }
+    
 }
