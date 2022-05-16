9a64bb57a6844134406173d7fb3ed2604f18b44d
yamodah <yassinedahlek@gmail.com>
2022-05-15 20:39:55 -0400

    isolated 'best car' and focusing on it

--- car.js
+++ car.js
@@ -1,5 +1,5 @@
 class Car {
-  constructor(x, y, width, height, controlType, maxSpeed=3) {
+  constructor(x, y, width, height, controlType, maxSpeed = 3) {
     this.x = x;
     this.y = y;
     this.width = width;
@@ -11,46 +11,43 @@ class Car {
     this.friction = 0.05;
     this.angle = 0;
     this.damaged = false;
-    this.useBrain =controlType=="AI"
+    this.useBrain = controlType == "AI";
 
-    if (controlType!="DUMMY"){
-        this.sensor = new Sensor(this);
-        this.brain = new NeuralNetwork(
-            [this.sensor.rayCount, 6, 4]
-        )
-
+    if (controlType != "DUMMY") {
+      this.sensor = new Sensor(this);
+      this.brain = new NeuralNetwork([this.sensor.rayCount, 6, 4]);
     }
     this.controls = new Controls(controlType);
   }
   update(roadBorders, traffic) {
-    if(!this.damaged){
-        this.#move();
-        this.polygon = this.#createPolygon();
-        this.damaged = this.#assessDamage(roadBorders, traffic);
+    if (!this.damaged) {
+      this.#move();
+      this.polygon = this.#createPolygon();
+      this.damaged = this.#assessDamage(roadBorders, traffic);
     }
-    if(this.sensor){
-        this.sensor.update(roadBorders, traffic);
-        const offsets = this.sensor.readings.map((s)=>s==null?0:1-s.offset)
-        const outputs = NeuralNetwork.feedForward(offsets,this.brain)
-        // console.log(outputs)
+    if (this.sensor) {
+      this.sensor.update(roadBorders, traffic);
+      const offsets = this.sensor.readings.map((s) =>
+        s == null ? 0 : 1 - s.offset
+      );
+      const outputs = NeuralNetwork.feedForward(offsets, this.brain);
+      // console.log(outputs)
 
-        if (this.useBrain){
-            this.controls.forward = outputs[0]
-            this.controls.left = outputs[1]
-            this.controls.right = outputs[2]
-            this.controls.reverse = outputs[3]
-        }
+      if (this.useBrain) {
+        this.controls.forward = outputs[0];
+        this.controls.left = outputs[1];
+        this.controls.right = outputs[2];
+        this.controls.reverse = outputs[3];
+      }
     }
   }
   #assessDamage(roadBorders, traffic) {
     for (let i = 0; i < roadBorders.length; i++) {
-
       if (polysIntersect(this.polygon, roadBorders[i])) {
         return true;
       }
     }
     for (let i = 0; i < traffic.length; i++) {
-
       if (polysIntersect(this.polygon, traffic[i].polygon)) {
         return true;
       }
@@ -118,7 +115,7 @@ class Car {
     this.x -= Math.sin(this.angle) * this.speed;
     this.y -= Math.cos(this.angle) * this.speed;
   }
-  draw(ctx, color) {
+  draw(ctx, color, drawSensor = false) {
     if (this.damaged) {
       ctx.fillStyle = "grey";
     } else {
@@ -130,8 +127,8 @@ class Car {
       ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
     }
     ctx.fill();
-    if(this.sensor){
-        this.sensor.draw(ctx);
+    if (this.sensor && drawSensor) {
+      this.sensor.draw(ctx);
     }
   }
 }
--- index.js
+++ index.js
@@ -6,31 +6,49 @@ networkCanvas.width = 300;
 const carCtx = carCanvas.getContext("2d");
 const networkCtx = networkCanvas.getContext("2d");
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS");
+
+const N = 100;
+const cars = generateCars(N);
 const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)];
 
 animate();
 
+function generateCars(N) {
+  const cars = [];
+  for (let i = 0; i <= N; i++) {
+    cars.push(new Car(road.getLaneCenter(1), 100, 30, 50, "AI"));
+  }
+  return cars;
+}
 function animate(time) {
   for (let i = 0; i < traffic.length; i++) {
-    traffic[i].update(road.borders,[]);
+    traffic[i].update(road.borders, []);
   }
-  car.update(road.borders, traffic);
+  for (let i = 0; i < cars.length; i++) {
+    cars[i].update(road.borders, traffic);
+  }
+  //car that is the highest up the canvas
+  const bestCar = cars.find((c) => c.y == Math.min(...cars.map((c) => c.y)));
   //move resize into animate functions like fillrect to clear the canvas
   carCanvas.height = innerHeight;
   networkCanvas.height = innerHeight;
   carCtx.save();
-  carCtx.translate(0, -car.y + carCanvas.height * 0.7);
+  carCtx.translate(0, -bestCar.y + carCanvas.height * 0.7);
   road.draw(carCtx);
   for (let i = 0; i < traffic.length; i++) {
-    traffic[i].draw(carCtx,"red");
+    traffic[i].draw(carCtx, "red");
   }
-  car.draw(carCtx,"blue");
+  carCtx.globalAlpha = 0.2;
+  for (let i = 0; i < cars.length; i++) {
+    cars[i].draw(carCtx, "blue");
+  }
+  carCtx.globalAlpha = 1;
+  bestCar.draw(carCtx, "blue", true);
 
   carCtx.restore();
 
-  networkCtx.lineDashOffset = -time/50
-  Visualizer.drawNetwork(networkCtx, car.brain)
+  networkCtx.lineDashOffset = -time / 50;
+  Visualizer.drawNetwork(networkCtx, bestCar.brain);
 
   requestAnimationFrame(animate);
 }
