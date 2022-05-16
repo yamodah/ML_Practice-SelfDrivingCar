51564329d77af56b41a4ac5a7b165b5104d4671b
yamodah <yassinedahlek@gmail.com>
2022-05-16 10:58:25 -0400

    raised varience in cars to 10%

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ if (localStorage.getItem("bestBrain")) {
   for (let i = 0; i < cars.length; i++) {
     cars[i].brain = JSON.parse(localStorage.getItem("bestBrain"));
     if (i != 0) {
-      NeuralNetwork.mutate(cars[i].brain, 0.01);
+      NeuralNetwork.mutate(cars[i].brain, 0.1);
     }
   }
 }
